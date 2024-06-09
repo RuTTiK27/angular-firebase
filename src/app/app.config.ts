7c7db6b333cbfaefd8f8a9bfab1d44b30ea4c086
Rutvik Pandya <rutvikpandya00@gmail.com>
2024-06-09 09:07:50 +0530

    just for update cli

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,15 +7,10 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), 
+  providers: [
+    provideRouter(routes), 
     provideClientHydration(), 
-    provideFirebaseApp(() => initializeApp({"projectId":"ang-fire-137f5",
-    "appId":"1:790990143121:web:d2a1d0669e378102190930",
-    "storageBucket":"ang-fire-137f5.appspot.com",
-    "apiKey":"AIzaSyCK_n4sJhHLjptvT0UCERBM-z9xGAzyupI",
-    "authDomain":"ang-fire-137f5.firebaseapp.com",
-    "messagingSenderId":"790990143121"})), 
-    provideFirestore(() => getFirestore()),
+    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({"projectId":"ang-fire-137f5","appId":"1:790990143121:web:d2a1d0669e378102190930","storageBucket":"ang-fire-137f5.appspot.com","apiKey":"AIzaSyCK_n4sJhHLjptvT0UCERBM-z9xGAzyupI","authDomain":"ang-fire-137f5.firebaseapp.com","messagingSenderId":"790990143121"})), provideFirestore(() => getFirestore()),
     
   ]
 };
